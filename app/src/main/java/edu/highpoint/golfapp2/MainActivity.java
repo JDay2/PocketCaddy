aacf7a09fb41e741cc6863027282fc41a197a632
beccakorn <61066214+beccakorn@users.noreply.github.com>
2021-11-11 12:06:50 -0500

    see if this works

--- app/src/main/java/edu/highpoint/golfapp2/MainActivity.java
+++ app/src/main/java/edu/highpoint/golfapp2/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -120,7 +121,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void goToSearch(){
+    public void goToSearch(View view){
         Intent intent = new Intent(this, SearchActivity.class);
         startActivity(intent);
     }
