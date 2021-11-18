c310496feab7d0981d5c0ebc49f13352a0c2b234
beccakorn <61066214+beccakorn@users.noreply.github.com>
2021-11-18 12:44:12 -0500

    update to home/search page looks

--- app/src/main/java/edu/highpoint/golfapp2/SearchActivity.java
+++ app/src/main/java/edu/highpoint/golfapp2/SearchActivity.java
@@ -2,13 +2,52 @@ package edu.highpoint.golfapp2;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
-import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SearchActivity extends AppCompatActivity{
+
+    Button buttonUp;
+    Button buttonDown;
+    Integer sqmi;
+    EditText editsqmi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_activity);
+
+        buttonDown=(Button) findViewById(R.id.downButton);
+        buttonUp=(Button) findViewById(R.id.upButton);
+        sqmi=10;
+        editsqmi=(EditText)findViewById(R.id.editSqMi);
+        editsqmi.setText(sqmi.toString());
+    }
+
+
+    public void goUp(View view){
+
+        int sqmi=Integer.parseInt(editsqmi.getText().toString());
+
+        if(sqmi<100)
+            sqmi++;
+        else if(sqmi>100)
+            sqmi=99;
+
+        editsqmi.setText(sqmi); //should be sqmi.toString() but that is bugging for some reason
+    }
+
+    public void goDown(View view){
+
+        int sqmi=Integer.parseInt(editsqmi.getText().toString());
+
+        if(sqmi>1)
+            sqmi--;
+        else if(sqmi<1)
+            sqmi=1;
+
+        editsqmi.setText(sqmi); //should be sqmi.toString() but that is bugging for some reason
     }
 }
