b88f6524e7b592c2b957951246337783944e4ccc
beccakorn <61066214+beccakorn@users.noreply.github.com>
2021-12-09 13:03:11 -0500

    final ui stuff

--- app/src/main/java/edu/highpoint/golfapp2/StatsActivity.java
+++ app/src/main/java/edu/highpoint/golfapp2/StatsActivity.java
@@ -1,9 +1,12 @@
 package edu.highpoint.golfapp2;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StatsActivity extends AppCompatActivity {
@@ -15,12 +18,12 @@ public class StatsActivity extends AppCompatActivity {
 
     public void calcStats(View view){
 
-        enterShot();
+        enterShot(view);
 
         Intent intent = new Intent(this, StatsFinalActivity.class);
         startActivity(intent);
     }
-    public void enterShot() {
+    public void enterShot(View view) {
 
         RadioButton left = (RadioButton) findViewById(R.id.leftButton);
         RadioButton right = (RadioButton) findViewById(R.id.rightButton);
@@ -61,9 +64,12 @@ public class StatsActivity extends AppCompatActivity {
         else{
             curve.setChecked(false);
             offTarget.setChecked(false);
+            Toast.makeText(StatsActivity.this, "Cannot enter shot.", Toast.LENGTH_SHORT).show();
         }
     }
-    public void openPopUp(){
+    public void openPopUp(View view){
+
+        Toast.makeText(StatsActivity.this, "Thin: hit top half of ball. Chunky: caught too much ground.", Toast.LENGTH_LONG).show();
 
     }
 }
